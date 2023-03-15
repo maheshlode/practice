effac5d3fcf04c11c1a2e862f847d4bdc129ecab
maheshlode <maheshlode55@gmail.com>
2023-03-15 13:28:09 +0530

    logo change and routing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   const [mode,setMode]=useState('light');
   const [alert,setAlert]=useState(null);
@@ -22,21 +29,29 @@ function App() {
     if(mode==="light"){
       setMode("dark");
       document.body.style.backgroundColor='#042743';
-      showAlert("Dark mode has been enabled","success")
+      showAlert("Dark mode has been enabled","success");
+      document.title='TextUtils-dark mode';
     }else{
       setMode("light");
       document.body.style.backgroundColor='white';
-      showAlert("Light mode has been enabled","success")
+      showAlert("Light mode has been enabled","success");
+      document.title='TextUtils-light mode';
     }
   }
   return (
     <>
+    <Router>
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert}/>
       <div className="container">
-        <TextForm showAlert={showAlert} heading="Enter your text to analyze" mode={mode}/>
-      </div>
-      <About/>
+          <Routes>
+            <Route path="/about" element={<About />}>
+            </Route>
+            <Route path="/" element={<TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert} />}>
+            </Route>
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
