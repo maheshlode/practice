1128e96f1618f7af6f608c6a2b6391faf5d972d7
maheshlode <maheshlode55@gmail.com>
2023-03-16 12:48:05 +0530

    fixes issue

--- src/components/About Us.js
+++ src/components/About Us.js
@@ -1,48 +1,35 @@
-import React, { useState } from "react";
+import React, {  } from "react";
 
-function About() {
+function About(props) {
 
-    const [myStyle, setMyStyle] = useState({
-        color: 'black',
-        backgroundColor: 'white'
-    })
+    // const [myStyle, setMyStyle] = useState({
+    //     color: 'black',
+    //     backgroundColor: 'white'
+    // })
 
-    const [btnText, setBtnText] = useState("Enable Dark Mode");
-
-    const enableDarkMode = () => {
-        if (myStyle.color === 'black') {
-            setMyStyle({
-                color: 'white',
-                backgroundColor: 'black',
-                border: '1px solid white',
-            })
-            setBtnText("Enable Light Mode");
-        } else {
-            setMyStyle({
-                color: 'black',
-                backgroundColor: 'white',
-            })
-            setBtnText("Enable Dark Mode");
-        }
+    let myStyle = {
+        color:props.mode === 'dark'?'white':'#042743',
+        backgroundColor:props.mode === 'dark'?'rgb(36,74,104)':'white'
     }
+
     return (
         <div className="container">
-            <h2>About Us</h2>
+            <h2 style={{color:props.mode === 'dark'?'white':'black'}}>About Us</h2>
             <div className="accordion" id="accordionExample" style={myStyle}>
                 <div className="accordion-item" style={myStyle}>
-                    <h2 className="accordion-header" id="headingOne" >
-                        <button className="accordion-button" type="button" style={myStyle} data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <h2 className="accordion-header" id="headingOne" style={myStyle} >
+                        <button className="accordion-button" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                             About Us
                         </button>
                     </h2>
-                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div id="collapseOne" className="accordion-collapse collapse show" style={myStyle} aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                         <div className="accordion-body">
                             <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                         </div>
                     </div>
                 </div>
-                <div className="accordion-item">
-                    <h2 className="accordion-header" id="headingTwo">
+                <div className="accordion-item" style={myStyle}>
+                    <h2 className="accordion-header" style={myStyle}  id="headingTwo">
                         <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                             Accordion Item #2
                         </button>
@@ -66,9 +53,9 @@ function About() {
                     </div>
                 </div>
             </div>
-            <div className="container my-2">
+            {/* <div className="container my-2">
                 <button type="button" onClick={enableDarkMode} className="btn btn-primary">{btnText}</button>
-            </div>
+            </div> */}
         </div>
     )
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,34 +6,35 @@ function TextForm(props) {
         //console.log("Uppercase button clicked");
         let newText = text.toUpperCase();
         setText(newText);
-        props.showAlert("Converted to Uppercase!","success")
+        props.showAlert("Converted to Uppercase!", "success")
     }
 
-    const handleLowerCase=()=>{
-        let newText=text.toLowerCase();
+    const handleLowerCase = () => {
+        let newText = text.toLowerCase();
         setText(newText);
-        props.showAlert("Converted to Lowercase!","success")
+        props.showAlert("Converted to Lowercase!", "success")
     }
 
     // function to reverse the given text
-    const reverseString=()=>{
-        let newText=text.split("");
-        let reverseStr=newText.reverse("");
-        let joinStr=reverseStr.join("");
-        let newReverseText=joinStr;
+    const reverseString = () => {
+        let newText = text.split("");
+        let reverseStr = newText.reverse("");
+        let joinStr = reverseStr.join("");
+        let newReverseText = joinStr;
         //let newText=text.split("").reverse("").join("");
         setText(newReverseText);
     }
 
-    const handleCopy=()=>{
-        let text=document.getElementById("myBox");
+    const handleCopy = () => {
+        let text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
-        props.showAlert("Copied to clipboard!","success")
+        document.getSelection().removeAllRanges();
+        props.showAlert("Copied to clipboard!", "success")
     }
-    const clearText=()=>{
+    const clearText = () => {
         setText('');
-        props.showAlert("Text cleared!","success")
+        props.showAlert("Text cleared!", "success")
     }
 
     const handleOnChange = (event) => {
@@ -44,23 +45,23 @@ function TextForm(props) {
 
     return (
         <>
-            <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
-                <div className="form-group my-3">
+            <div  className="container" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
+                <div  className="form-group my-3">
                     <h2>{props.heading}</h2>
-                    <textarea className="form-control my-3" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="7"></textarea>
+                    <textarea  className="form-control my-3" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? '#13455e' : 'white', color: props.mode === 'dark' ? 'white' : '#042743' }} id="myBox" rows="7"></textarea>
                 </div>
-                <button className="btn btn-primary mx-2 my-2" onClick={handleUpperCase}>Convert to Uppercase</button>
-                <button className="btn btn-primary mx-2 my-2" onClick={handleLowerCase}>Convert to Lowercase</button>
-                <button className="btn btn-primary mx-2 my-2" onClick={clearText}>Clear Text</button>
-                <button className="btn btn-primary mx-2 my-2" onClick={reverseString}>Reverse Text</button>
-                <button className="btn btn-primary mx-2 my-2" onClick={handleCopy}>Copy Text</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleUpperCase}>Convert to Uppercase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleLowerCase}>Convert to Lowercase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={reverseString}>Reverse Text</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={handleCopy}>Copy Text</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={clearText}>Clear Text</button>
             </div>
-            <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+            <div  className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
                 <h1>your text summary</h1>
-                <p>{text.split(" ").length} words and {text.length}</p>
-                <p>{0.008 * text.split(" ").length} minutes to read the upper paragraph</p>
+                <p>{text.split(" ").filter((element) => { return element.length !== 0 }).length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").filter((element) => { return element.length !== 0 }).length} minutes to read the upper paragraph</p>
                 <h2>Preview</h2>
-                <p>{text.length>0?text:"Text Box is Empty"}</p>
+                <p>{text.length > 0 ? text : "Text Box is Empty"}</p>
             </div>
 
         </>
